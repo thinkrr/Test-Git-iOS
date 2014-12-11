4fff57b80e6769e060894d6ee6856dbe9abefb25
David Cao <cao@Davids-Mac-Mini.tx.rr.com>
2014-12-10 22:19:12 -0600

    added test class

---
+++ Git Test/DCGitTest.h
@@ -0,0 +1,13 @@
+//
+//  DCGitTest.h
+//  Git Test
+//
+//  Created by David Cao on 12/10/14.
+//  Copyright (c) 2014 Tutorial Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCGitTest : NSObject
+
+@end
---
+++ Git Test/DCGitTest.m
@@ -0,0 +1,13 @@
+//
+//  DCGitTest.m
+//  Git Test
+//
+//  Created by David Cao on 12/10/14.
+//  Copyright (c) 2014 Tutorial Code. All rights reserved.
+//
+
+#import "DCGitTest.h"
+
+@implementation DCGitTest
+
+@end
